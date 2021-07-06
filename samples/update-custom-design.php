e3d313026b40f9d7f5c46c47887e05f4a78402fa
Debian <debian@9ce9c5475ab2>
2021-07-06 10:28:16 +0000

    fix a sample

--- samples/update-custom-design.php
+++ samples/update-custom-design.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 # Update custom design of a channel
 require '../vendor/autoload.php';
 require '../config.php';
