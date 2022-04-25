f03786f8ca8974f40b3c0a41d491219a22c6abe4
Yann Lazerges <yann@blastream.com>
2022-04-25 11:30:17 +0200

    Fix initChannel

--- src/Space.php
+++ src/Space.php
@@ -6,7 +6,7 @@ trait Space
 {
     
     public function initChannel($result) {
-        $channel = new Channel($this->_public_key, $this->_public_key, $this->_whitelabel_url);
+        $channel = new Channel($this->_public_key, $this->_private_key, $this->_whitelabel_url);
         $channel->setRequestUrl($this->_request_url);
         $channel->setSlug($this->_slug);
         $channel->setResponseToken($result);
