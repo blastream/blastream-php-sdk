f94965805d614b197bea48eb4f6cbf1235be484e
Emmanuel Freard <emmanuel@streaminvaders.com>
2024-01-02 12:23:05 +0100

    change url for upload

--- src/Document.php
+++ src/Document.php
@@ -16,11 +16,16 @@ trait Document
     }
     
     public function uploadFromUrl($url){
-        return $this->post('/upload-from-url', [
+        $requestUrl = $this->_request_url;
+        if($this->_request_url == 'https://api.v2.blastream.com')
+            $this->_request_url = 'https://docs.v2.blastream.com';
+        $res = $this->post('/upload-from-url', [
             'body' => [            
                 'url' => $url
-            ]
+            ],
         ]);
+        $this->_request_url = $requestUrl;
+        return $res;
     }
 
 }
