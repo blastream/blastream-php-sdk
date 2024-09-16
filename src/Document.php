1be22ddbf962fea22c2d59fa8e252dc207a39bc1
Yann Lazerges <yann@blastream.com>
2024-09-16 15:18:08 +0000

    add getFile by id, set name for uploadFromUrl

--- src/Document.php
+++ src/Document.php
@@ -15,15 +15,17 @@ trait Document
         ]);
     }
     
-    public function uploadFromUrl($url){
+    public function uploadFromUrl($url, $param = []){
         $requestUrl = $this->_request_url;
+        $body = [           
+            'url' => $url
+        ];
+        if(isset($param['name'])){
+            $body['name'] = $param['name'];
+        }
         if($this->_request_url == 'https://api.v2.blastream.com')
             $this->_request_url = 'https://docs.v2.blastream.com';
-        $res = $this->post('/upload-from-url', [
-            'body' => [            
-                'url' => $url
-            ],
-        ]);
+        $res = $this->post('/upload-from-url',['body' => $body ]);
         $this->_request_url = $requestUrl;
         return $res;
     }
@@ -31,6 +33,10 @@ trait Document
     public function getFiles(){        
         return $this->get('/files');
     }
+
+    public function getFile($id){        
+        return $this->get('/file/'.$id);
+    }        
 
     public function updateFile($documentId, $name){        
         return $this->post('/document/' . $documentId, [
